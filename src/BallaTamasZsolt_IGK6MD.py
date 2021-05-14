70d1d015459a9179536d47bb77ed24eac55195c1
excraptor <tamasballa99@gmail.com>
2021-05-14 14:10:59 +0200

    finished project

--- src/BallaTamasZsolt_IGK6MD.py
+++ src/BallaTamasZsolt_IGK6MD.py
@@ -1,9 +1,7 @@
 import cv2 as cv
 import numpy as np
 import math as m
-from numpy.core.fromnumeric import sort
-
-from numpy.core.numeric import Infinity
+import sys
 
 def filter(array, precision=0):
     ret = set()
@@ -19,27 +17,14 @@ def addToSetWithThreshold(pSet, pValue, angleTreshold):
 def cluster(lines, angleTreshold):
     pointers = set()
     pointers.add(lines[0])
-    # print("in cluster: " + str(pointers))
-    # for pointer in pointers.copy():
-    #     for line in lines:
-    #         if(abs(line[0] - pointer[0]) > angleTreshold or abs(line[1] - pointer[1]) > lengthTreshold or line[2] != pointer[2]):
-    #             print(abs(line[0] - pointer[0]) > angleTreshold)
-    #             print("angle: " + str(line[0]) + " pointer: " + str(pointer[0]))
-    #             print("angletresh: " + str(abs(line[0] - pointer[0])) + ">" + str(angleTreshold))
-    #             print(abs(line[1] - pointer[1]) > lengthTreshold)
-    #             print(line[2] != pointer[2])
-    #             pointers.add(line)
     for line in lines:
         addToSetWithThreshold(pointers, line, angleTreshold)
     return pointers
 
-
-    
-
 def angleFromHorizontal(vector):
     y = vector[1] - vector[3]
     x = vector[0] - vector[2]
-    if(x is not 0.0):
+    if(x != 0.0):
         return m.degrees(m.atan(y*1.0/x))
     return 0.0
 
@@ -54,7 +39,7 @@ def vectorLength(vector):
 def whichQuarter(img, vector):
     distP1 = distanceFromMiddle(img, (vector[0], vector[1]))
     distP2 = distanceFromMiddle(img, (vector[2], vector[3]))
-    print("dist1: " + str(distP1) + ", dist2: " + str(distP2))
+    # print("dist1: " + str(distP1) + ", dist2: " + str(distP2))
     if(distP1 > distP2):
         x = vector[0]
         y = vector[1]
@@ -82,6 +67,8 @@ def minutes(angle, quarter):
         minute = abs(abs(angle)/6-15)+30
     elif(quarter is 4):
         minute = angle/6+45
+    if minute == 60:
+        minute = 0
     return minute
 
 def hour(angle, quarter):
@@ -93,6 +80,8 @@ def hour(angle, quarter):
         hour = abs(abs(angle)/30-3)+6
     elif(quarter is 4):
         hour = angle/30+9
+    if hour == 0:
+        hour = 12
     return hour
 
 def sortHelp(line):
@@ -100,86 +89,121 @@ def sortHelp(line):
 
 def time(pointers):
     ret = sorted(pointers, key=sortHelp)
-    print(ret)
+    # print(ret)
     return (round(hour(ret[0][0], ret[0][2])), round(minutes(ret[1][0], ret[1][2])))
 
-img = cv.imread('../Mennyi_az_ido/faliora11.jpg', cv.IMREAD_GRAYSCALE)
-cdst = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
-print(img.shape)
+def reinit():
+    global img, cdst, black, segment, canny
+    
+    cdst = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+    # print(img.shape)
+    img = cv.medianBlur(img, 7)
+    #cv.imshow('ora', img)
+    black = np.zeros(cdst.shape)
+    segment = cv.inRange(cdst, (0, 0, 0), (255, 255, 110))
+    #blur = cv.medianBlur(img, 11)
+    canny = cv.Canny(img, 50, 200)
+    # cv. imshow('canny', canny)
+    cv.imshow('image', img)
 
-img = cv.medianBlur(img, 7)
-cv.imshow('ora', img)
-black = np.zeros(cdst.shape)
+def recalculateLines():
+    global minLineLength, black
+    minutesCalculated = []
+    lengths = []
+    angles = []
+    lines = []
 
-segment = cv.inRange(cdst, (0, 0, 0), (255, 255, 110))
+    linesP = cv.HoughLinesP(segment, 1, np.pi/90, 50, None, minLineLength, 10) 
+    # print(len(linesP))
+    # with open('linesP.txt', 'w') as f:
+    #     f.write(str(linesP))
 
-# blur = cv.medianBlur(img, 11)
-canny = cv.Canny(img, 50, 200)
-# cv. imshow('canny', canny)
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+                l = linesP[i][0]
+                # print(l)
+                angle = angleFromHorizontal(l)
+                quarter = whichQuarter(black, l)
+                minute = minutes(angle, quarter)
+                length = vectorLength(l)
+                minutesCalculated.append(minute)
+                lengths.append(length)
+                angles.append(angle)
+                lines.append((angle, length, quarter))
+                
+                cv.line(black, (l[0], l[1]), (l[2], l[3]), (0, 255, 0), 3, cv.LINE_AA)
 
-# lines = cv.HoughLines(segment, 1, np.pi/180, 100, None, 0, 0)
-# print('detektalt egyenesek szama: ', len(lines))
+    pointers = []
+    if len(lines) != 0:
+        pointers = cluster(lines, 3)
 
-# sizemax = m.sqrt(canny.shape[0] ** 2 + canny.shape[1] ** 2)
-# for i in range(len(lines)):
-#     rho = lines[i][0][0]
-#     theta = lines[i][0][1]
-#     a = m.cos(theta)
-#     b = m.sin(theta)
-#     x0 = a*rhofali
-#     y0 = b*rho
-#     pt1 = (int(x0 + sizemax * (-b)), int(y0 + sizemax * a))
-#     pt2 = (int(x0 - sizemax * (-b)), int(y0 - sizemax * a)) 
-#     cv.line(cdst, pt1, pt2, (0, 0, 255), 3, cv.LINE_AA)
+    if(len(pointers) > 3 or len(pointers) < 2):
+        print("Can't tell the time!")
+    else:
+        timeOnClock = time(pointers)
+        print(str(timeOnClock[0])+":" + str(timeOnClock[1]))
 
+def setMinLineLength(trackPos):
+    global minLineLength, black, cdst
+    minLineLength = trackPos
+    black = np.ndarray(cdst.shape)
+    #cv.imshow('black', black)
+    recalculateLines()
 
-linesP = cv.HoughLinesP(segment, 1, np.pi/90, 50, None, 220, 10) 
-print(len(linesP))
-with open('linesP.txt', 'w') as f:
-    f.write(str(linesP))
+def changePicture(pic):
+    global img
+    img = cv.imread('../Mennyi_az_ido/' + pic, cv.IMREAD_GRAYSCALE)
+    reinit()
 
-minutesCalculated = []
-lengths = []
-angles = []
-lines = []
-for i in range(0, len(linesP)):
-        l = linesP[i][0]
-        print(l)
-        angle = angleFromHorizontal(l)
-        quarter = whichQuarter(black, l)
-        minute = minutes(angle, quarter)
-        length = vectorLength(l)
-        minutesCalculated.append(minute)
-        lengths.append(length)
-        angles.append(angle)
-        lines.append((angle, length, quarter))
-        print('angle: ' + str(round(angle, 3)))
-        print('quarter: ' + str(quarter))
-        print('minute: '+ str(minute))
-        
-        cv.line(black, (l[0], l[1]), (l[2], l[3]), (0, 255, 0), 3, cv.LINE_AA)
-
-print("unique minutes: " + str(filter(minutesCalculated)))
-print("unique lengths: " + str(filter(lengths, 2)))
-print("unique angles :" + str(filter(angles, 0)))
-print("lines: " + str(lines))
-pointers = cluster(lines, 3)
-print("number of pointers: " + str(len(pointers)))
-print("pointers: " + str(pointers))
-print("################ TIME ################")
-timeOnClock = time(pointers)
-print(str(timeOnClock[0])+":" + str(timeOnClock[1]))
-#l = linesP[3][0]
-# cv.line(black, (l[0], l[1]), (l[2], l[3]), (255, 0, 0), 3, cv.LINE_AA)
-
-# print('first quarter angle: ' + str(angleFromHorizontal([150, 150, 140, 160])))
-# print('second quarter angle:' + str(angleFromHorizontal([150, 150, 160, 160])))
-# print('third quarter angle:' + str(angleFromHorizontal([150, 150, 160, 140])))
-# print('fourth quarter angle:' + str(angleFromHorizontal([150, 150, 140, 140])))
+def test():
+    global minLineLength
+    print('faliora.jpg:')
+    changePicture('faliora.jpg')
+    setMinLineLength(50)
+    print('faliora5.jpg')
+    changePicture('faliora5.jpg')
+    setMinLineLength(100)
+    print('faliora7.jpg')
+    changePicture('faliora7.jpg')
+    setMinLineLength(50)
+    print('faliora9.png')
+    changePicture('faliora9.png')
+    setMinLineLength(50)
+    print('faliora11.jpg')
+    changePicture('faliora11.jpg')
+    setMinLineLength(220)
+    print('sp_noise.jpg')
+    changePicture('sp_noise.jpg')
+    setMinLineLength(200)
+    
 
 
-cv.imshow('segment', canny)
-cv.imshow('hough', cdst)
-cv.imshow('black', black)
-cv.waitKey()
+try:
+    if sys.argv[1] == 'test':
+        test()
+except:
+    img = cv.imread('../Mennyi_az_ido/faliora11.jpg', cv.IMREAD_GRAYSCALE)
+    minLineLength = 220
+    reinit()
+    cv.namedWindow('GUI')
+    cv.createTrackbar('min. line length', 'GUI', 1, 300, setMinLineLength)
+    recalculateLines() 
+
+    while True:
+        key = cv.waitKey()
+        if key & 0xFF == ord('q') or key == 27:
+            break
+        if key & 0xFF == ord('1'):
+            changePicture('faliora.jpg')
+        if key & 0xFF == ord('2'):
+            changePicture('faliora5.jpg')
+        if key & 0xFF == ord('3'):
+            changePicture('faliora9.png')
+        if key & 0xFF == ord('4'): 
+            changePicture('faliora7.jpg')
+        if key & 0xFF == ord('5'): 
+            changePicture('faliora11.jpg')
+        if key & 0xFF == ord('6'):
+            changePicture('sp_noise.jpg')
+
 cv.destroyAllWindows()
---
+++ src/zajositas.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import numpy as np
+import random
+# SOURCE: STACKOVERFLOW
+# not that difficult though
+def sp_noise(image,prob):
+    '''
+    Add salt and pepper noise to image
+    prob: Probability of the noise
+    '''
+    output = np.zeros(image.shape,np.uint8)
+    thres = 1 - prob 
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            elif rdn > thres:
+                output[i][j] = 255
+            else:
+                output[i][j] = image[i][j]
+    return output
+
+image = cv.imread('../Mennyi_az_ido/faliora11.jpg',0) # Only for grayscale image
+noise_img = sp_noise(image,0.05)
+cv.imwrite('sp_noise.jpg', noise_img)
